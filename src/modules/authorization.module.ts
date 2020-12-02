b2ca6d9d24f337dc1657bbe08aed26f456b2f35f
kukjevov <kukjevov@gmail.com>
2020-12-02 13:23:10 +0100

    INT: fixed adding letAuthorizedDirective to module

--- src/modules/authorization.module.ts
+++ src/modules/authorization.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {AuthorizeDirective} from '../common/authorize.directive';
+import {LetAuthorizedDirective} from '../common/letAuthorized.directive';
 
 /**
  * Module for authorization 
@@ -9,8 +10,8 @@ import {AuthorizeDirective} from '../common/authorize.directive';
 @NgModule(
 {
     imports: [CommonModule],
-    declarations: [AuthorizeDirective],
-    exports: [AuthorizeDirective]
+    declarations: [AuthorizeDirective, LetAuthorizedDirective],
+    exports: [AuthorizeDirective, LetAuthorizedDirective]
 })
 export class AuthorizationModule
 {
